b8873a547de0053c2df4487f92cda8ee38f6bcd8
adel14524 <nuradilah778@gmail.com>
2022-04-13 02:08:33 +0800

    Update top-nav.php

--- top-nav.php
+++ top-nav.php
@@ -1,11 +1,19 @@
 <head>
+	<!-- Required meta tags -->
 	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=PXqROxUpVadqewYbMmVBBewZKxulONyCjhO5V1FtVUSi34DAO-yhzNToSd_Jjrx_F7kLDeqkE7yuBxeb-H8rZQnLBtDAnMb23ctCXDMxAS_eGy_2UqOBBGGEYkyF_bfokmb0HxJeHjrkYJUfZThtRKNYUdZaGa0gjQqLitlC-9E0J4tW4YWozWuRgWa14kVfet-DDx0aIMI0zeOPULi28JGBp3Llc4OSut76MZrURZvK8dm1YFGnsoOCBPYvnJTkv5ba8EZOEoqqcSi4qlcEl6bmbKA80hl7YlXf-MiKNO4eCFLTeO867oTcFhEiSY-a7CCyBvD_t0J5JSEZDBuO7UGpoIK6EllHVv6sRiuSmwY8r5P2YElE67v9bAvePwmA_lbKbFriMAxooE07fXS0kFsq5qoJcAdhzB__w6qKrJ7NVGeLE3cMand-sXyxnAFRl7zxhlb1Y5iWvIyZQY6Qjg" charset="UTF-8"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=PXqROxUpVadqewYbMmVBBewZKxulONyCjhO5V1FtVUSi34DAO-yhzNToSd_Jjrx_F7kLDeqkE7yuBxeb-H8rZQnLBtDAnMb23ctCXDMxAS_eGy_2UqOBBGGEYkyF_bfokmb0HxJeHjrkYJUfZThtRKNYUdZaGa0gjQqLitlC-9E0J4tW4YWozWuRgWa14kVfet-DDx0aIMI0zeOPULi28JGBp3Llc4OSut76MZrURZvK8dm1YFGnsoOCBPYvnJTkv5ba8EZOEoqqcSi4qlcEl6bmbKA80hl7YlXf-MiKNO4eCFLTeO867oTcFhEiSY-a7CCyBvD_t0J5JSEZDBuO7UGpoIK6EllHVv6sRiuSmwY8r5P2YElE67v9bAvePwmA_lbKbFriMAxooE07fXS0kFsq5qoJcAdhzB__w6qKrJ7NVGeLE3cMand-sXyxnAFRl7zxhlb1Y5iWvIyZQY6Qjg" charset="UTF-8"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<style>
 		.right-navbar{
 			float:right;
